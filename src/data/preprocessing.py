d562879b197ace1c5fcdb69bdaaa341bfef2c7a7
ruslan <pringls2004@mail.ru>
2025-09-02 01:12:54 +0300

    Небольшие поправки

--- src/data/preprocessing.py
+++ src/data/preprocessing.py
@@ -2,8 +2,14 @@ import pandas as pd
 import numpy as np
 from typing import Tuple
 
+import sys
+import os
+
+ROOT_DIR = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+sys.path.append(ROOT_DIR)
+
 from cleaning import get_median, replacing_zero_values_median, get_quantiles, capping_outliers
-from features.engineering import create_medical_features, one_hot_encoding
+from src.features.engineering import create_medical_features, one_hot_encoding
 from scaling import get_params_for_standardization, standardization
 
 class DataPreprocessing:
@@ -21,14 +27,14 @@ class DataPreprocessing:
         '''Полная обработка данных'''
 
         # заполнение пропусков
-        columns_insert_medians = ['Glucose', 'BloodPressure', 'SkinThikness', 'Insulin', 'BMI']
+        columns_insert_medians = ['Glucose', 'BloodPressure', 'SkinThickness', 'Insulin', 'BMI']
         self.medians = get_median(X_train, columns_insert_medians)
 
         X_train = replacing_zero_values_median(X_train, columns_insert_medians, self.medians)
         X_test = replacing_zero_values_median(X_test, columns_insert_medians, self.medians)
 
         # capping(обрезание)
-        columns_capping = ['Pregnancies', 'SkinThikness', 'Insulin']
+        columns_capping = ['Pregnancies', 'SkinThickness', 'Insulin']
         quantiles = [0.99] * 3
         quantiles_values = get_quantiles(X_train, columns_capping, quantiles)
         inequality = ['>'] * 3
@@ -41,10 +47,10 @@ class DataPreprocessing:
         X_test = one_hot_encoding(create_medical_features(X_test))
 
         # standardization data
-        columns_get_mean_std = ['Pregnancies', 'SkinThikness', 'Insulin', 'DiabetesPedigreeFunction', 'Age', 'HOMA_IR', 'BMI_AGE_BOND']
+        columns_get_mean_std = ['Pregnancies', 'SkinThickness', 'Insulin', 'DiabetesPedigreeFunction', 'Age', 'HOMA_IR', 'BMI_AGE_BOND']
         self.standard_data = get_params_for_standardization(X_train, columns_get_mean_std)
 
         X_train = standardization(X_train, self.standard_data)
         X_test = standardization(X_test, self.standard_data)
 
-        return X_test, X_test
+        return X_train, X_test
